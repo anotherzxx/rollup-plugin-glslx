26b0e782f9506c8d0018e60ed4438d52b5180549
zxx <30956192+anotherzxx@users.noreply.github.com>
2022-03-28 23:54:51 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# rollup-plugin-glslx
+
+Import GLSL/GLSLX files in [rollup](https://github.com/rollup/rollup) and [vite](https://github.com/vitejs/vite) using [glslx](https://github.com/evanw/glslx).
---
+++ index.js
@@ -0,0 +1,34 @@
+import './glslx.js'
+
+/** @type {import('glslx')} */
+globalThis.GLSLX
+
+let dev = process.env.NODE_ENV != 'production'
+
+export default (/** @type {import('glslx').CompileArgs} */ args) =>
+  /** @type {import('rollup').Plugin} */
+  ({
+    name: 'glslx',
+
+    transform(code, id) {
+      if (!/((glslx?)|(vert)|(frag))$/.test(id)) return code
+
+      let glslx = id.endsWith('x')
+
+      args = args || {
+        format: 'js',
+        renaming: dev || !glslx ? 'none' : 'all',
+        disableRewriting: dev,
+        prettyPrint: dev,
+        keepSymbols: dev,
+      }
+
+      let result = GLSLX.compile({ name: id, contents: code }, args)
+
+      if (!result.output) throw result.log
+
+      return glslx
+        ? result.output
+        : result.output + '\nexport default GLSLX_SOURCE_MAIN\n'
+    },
+  })
